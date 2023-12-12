6e95d0a840f68c7d538d906587b4b1de4979ba66
Rawan Adel <90906384+RawanA92@users.noreply.github.com>
2023-12-13 00:41:37 +0300

    Delete src directory

--- src/musics.java
+++
@@ -1,5 +0,0 @@
-public class musics {
-    static Music main_music = new Music("assets//sound//chicken dance song.wav" , true),
-    game_over = new Music("assets/sound/GameOver_Sound.wav",false ) ;
-
-}
